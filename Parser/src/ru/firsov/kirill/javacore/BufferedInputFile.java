478ba63be9d5740260ef3ecea5b10db46c968f12
Kirill Firsov <kifirigor@gmail.com>
2018-07-24 12:30:54 +0300

    Add files via upload

---
+++ Parser/src/ru/firsov/kirill/javacore/BufferedInputFile.java
@@ -0,0 +1,19 @@
+package ru.firsov.kirill.javacore;
+import java.io.*;
+
+public class BufferedInputFile {
+    public static void main(String[] args) {
+        try {
+            File file = new File("C:/Users/User/Desktop/die Arbeit/89021_05.01.17_SYL4449_KHV_UUS.bin");
+            FileReader fr = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fr);
+            String line = reader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+} /* (Execute to see output) *///:~
